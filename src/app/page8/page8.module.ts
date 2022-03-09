0c8fda067419a348cd4335eb2d1d24ba5b674497
Magnomus <franciscomagnom632@gmail.com>
2022-03-09 16:18:07 -0300

    mensagem

---
+++ src/app/page8/page8.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { Page8PageRoutingModule } from './page8-routing.module';
+
+import { Page8Page } from './page8.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    Page8PageRoutingModule
+  ],
+  declarations: [Page8Page]
+})
+export class Page8PageModule {}
